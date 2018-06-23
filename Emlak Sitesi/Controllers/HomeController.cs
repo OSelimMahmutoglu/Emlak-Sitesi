66573ac74107900a6677a6d65b631d9e0e200677
Ömer Selim Mahmutoğlu <osmahmutoglu@gmail.com>
2018-06-23 14:51:12 +0300

    FikirtepeKentselEmlakSitesi

---
+++ Emlak Sitesi/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using Emlak_Sitesi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using static System.Net.Mime.MediaTypeNames;
+using System.Runtime.Serialization.Json;
+using Newtonsoft.Json;
+
+namespace Emlak_Sitesi.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public void DosyayaYaz()
+        {
+            Ilan İlan = new Ilan()
+            {
+                ID = Guid.NewGuid(),
+                Baslik = "BAŞLIK",
+                AdresAciklama = "ADRESS",
+                BuyuklukM = 5,
+                EmlakTipi = "aRSA",
+                EklemeTarihi = DateTime.Now,
+                IlaniVeren = "öMER sELİM",
+                Fiyat = 1213,
+                Ilce = "fİKİRTEPE",
+                SatisTipi = "SATILIK",
+                IlansAciklama = "İLAN AÇIKLAMASI",
+                OdaSayisi = "",
+                YayindaMi = true,
+                MetaAbstract = "",
+                MetaDescription = "",
+                Metakeywords = ""
+            };
+
+            var json = new JavaScriptSerializer().Serialize(İlan);
+
+            string path = Server.MapPath("~/wwwroot/");
+
+
+
+            StreamWriter notDefteri = new StreamWriter(path + "Note.JSON", true);
+            notDefteri.Write(json);
+            notDefteri.Close();
+        }
+
+        
+        public ActionResult Index()
+        {
+            string path = Server.MapPath("/wwwroot");
+            var json = System.IO.File.ReadAllText(path + "/ilan.txt");
+            var result = JsonConvert.DeserializeObject<List<Ilan>>(json);
+            return View(result);
+        }
+
+        public ActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+    }
+}
---
+++ Emlak Sitesi/Models/Ilan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Emlak_Sitesi.Models
+{
+    public class Ilan
+    {
+        public Guid ID { get; set; }
+        public string Baslik { get; set; }
+        public string AdresAciklama { get; set; }
+        public decimal Fiyat { get; set; }
+        public double BuyuklukM { get; set; }
+        public string IlansAciklama { get; set; }
+        public DateTime EklemeTarihi { get; set; }
+        public string MetaAbstract { get; set; }
+        public string MetaDescription { get; set; }
+        public string Metakeywords { get; set; }
+        public string OdaSayisi { get; set; }
+        public bool YayindaMi { get; set; }
+        public string Ilce { get; set; }
+        public string IlaniVeren { get; set; }
+        public string EmlakTipi { get; set; }
+        public string SatisTipi { get; set; }
+    }
+    
+
+}
